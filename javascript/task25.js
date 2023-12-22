4bfaff54f34ddc099f6af0696c0b59265bc73b4b
jhansi205 <jhansi205@gmail.com>
2023-12-22 18:32:26 +0530

    deploy files

---
+++ javascript/task25.js
@@ -0,0 +1,34 @@
+// var i;//declartion 
+// i=1;//assignment
+// var num1=2;//initialization
+// var num1=3;//re-initialization
+// console.log(i);
+// console.log(num1);
+// var num1=90071992547409932456n+90071992547409932456n
+// console.log(num1);
+// var str1="jhansi";
+// var str2="lakshmi";
+// console.log(str1+str2);
+// var str1="jhansi";
+// var str="bhai";
+// var str2=`${str1} 
+// lakshmi ${str}`;
+// console.log(str2);
+// var obj={
+//     id:123,
+//     name:'chair',
+//     color:'brown'
+// };
+// // console.log(obj);
+// // console.log(obj.name);
+// console.log(`id is ${obj.id} & ${obj.color}`);
+// var num1=5**3;
+// console.log(num1);//powerof
+var i=2;
+// console.log(typeof i);
+// console.log(typeof typeof i);
+// console.log(++i);
+// console.log(i++);
+// console.log(++i+i++);//6
+// console.log(i++ + ++i);//6
+// console.log(++i);//5
